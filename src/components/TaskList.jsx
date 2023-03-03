665ed96cd0e94f0b9169c5f4922bbe5a5ae2f602
sainamercy <sainamercy@gmail.com>
2023-03-03 16:33:35 +0300

    feat: :sparkles: implement fetching tasks

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,20 +1,71 @@
-import React, { useContext } from "react";
+import React, { useEffect, useState } from "react";
 import { useAuthContext } from "../providers/Auth.provider";
+import axios from "axios";
+import { API_URL } from "../constants";
 
 const TaskList = () => {
-  const auth = useAuthContext();
+  const { user } = useAuthContext();
+  const [tasks, setTasks] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-  console.log(auth);
 
-  const logOut = () => {
-    localStorage.removeItem("user");
+  const getTasks = async () => {
+    axios
+      .get(`${API_URL}/tasks`, {
+        headers: {
+          "X-User-ID": user?.id,
+        },
+      })
+      .then((response) => {
+        setTasks(response.data.data);
+        navigate("/tasklist");
+      })
+      .catch((error) => {
+        // TODO: handle error
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
   };
+
+  useEffect(() => {
+    getTasks();
+  }, []);
+
+  // TODO:  Create todoItem component 
+  // TODO:  Handle Edit / Update Todo item
+  // TODO:  Handle Delete Todo item
+  // TODO:  Group Todo items per status - IN progress/todo/done etec 
+  // TODO: Create a view for all Todo Items and keep a state to swicth between grouped tasks view and all taska view 
+
+
+
 
   return (
     <div className="w-full h-full bg-gray-100 flex flex-col items-center gap-10 text-gray-800 text-2xl md:w-5/6">
-      <h1 onClick={logOut} className="cursor-pointer">
-        Task List for {auth?.user?.email}{" "}
-      </h1>
+      <h1 className="cursor-pointer">Task List for {user?.email} </h1>
+
+
+      {tasks?.length >  0 && (
+        <>
+        {tasks.map((task) => (
+          <div key={task.id} className="flex flex-col items-center gap-10">
+            
+            <h3> 
+            {task.title}
+            </h3>
+            <p>
+              {task.description}
+            </p>
+
+          </div>
+
+        ))}
+      </>
+      )}
+
+
+
     </div>
   );
 };
